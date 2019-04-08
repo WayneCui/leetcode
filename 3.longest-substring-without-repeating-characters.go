8de7e26af37ede74b5fa47568f595dbe82df2fac
WayneCui <zhuxiaozhu8167@163.com>
2019-04-08 12:41:03 +0800

    FEAT: add 3.longest-substring-without-repeating-characters.go

---
+++ 3.longest-substring-without-repeating-characters.go
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=3 lang=golang
+ *
+ * [3] Longest Substring Without Repeating Characters
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (28.14%)
+ * Total Accepted:    863.2K
+ * Total Submissions: 3.1M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * Given a string, find the length of the longest substring without repeating
+ * characters.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "abcabcbb"
+ * Output: 3 
+ * Explanation: The answer is "abc", with the length of 3. 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3. 
+ * ⁠            Note that the answer must be a substring, "pwke" is a
+ * subsequence and not a substring.
+ * 
+ * 
+ * 
+ * 
+ */
+func lengthOfLongestSubstring(s string) int {
+	size := len(s)
+	longest := 0
+	for i := 0; i < size; i++ {
+		record := make(map[byte]int)
+		curr := 0
+		for j := i; j < size; j++ {
+			_, ok := record[s[j]]
+			if ok {
+				break
+			} else {
+				record[s[j]] = j
+				curr = curr + 1
+			}
+		}
+
+		if longest < curr {
+			longest = curr
+		}
+	}
+
+	return longest
+}
+
