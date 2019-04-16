b7ec5b2ef1e713e3e4290ba4e97cbd6183da8458
WayneCui <zhuxiaozhu8167@163.com>
2019-04-16 19:18:32 +0800

    FEAT: add 240.search-a-2-d-matrix-ii.go

---
+++ 240.search-a-2-d-matrix-ii.go
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=240 lang=golang
+ *
+ * [240] Search a 2D Matrix II
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (40.48%)
+ * Total Accepted:    170.8K
+ * Total Submissions: 420.9K
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
+ *
+ * Write an efficient algorithm that searches for a value in an m x n matrix.
+ * This matrix has the following properties:
+ * 
+ * 
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * 
+ * 
+ * Example:
+ * 
+ * Consider the following matrix:
+ * 
+ * 
+ * [
+ * ⁠ [1,   4,  7, 11, 15],
+ * ⁠ [2,   5,  8, 12, 19],
+ * ⁠ [3,   6,  9, 16, 22],
+ * ⁠ [10, 13, 14, 17, 24],
+ * ⁠ [18, 21, 23, 26, 30]
+ * ]
+ * 
+ * 
+ * Given target = 5, return true.
+ * 
+ * Given target = 20, return false.
+ * 
+ */
+func searchMatrix(matrix [][]int, target int) bool {
+	m := len(matrix)
+	if m == 0 {
+		return false
+	}
+
+	n := len(matrix[0])
+	if n == 0 {
+		return false
+	}
+
+	row := m - 1
+	col := 0
+
+	for row >= 0 && col <= n - 1 {
+		if target == matrix[row][col] {
+			return true
+		} else if target > matrix[row][col] {
+			col += 1
+		} else {
+			row -= 1
+		}
+	}
+
+	return false
+}
+
