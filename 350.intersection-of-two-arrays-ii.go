423e9546639677382681b0dee348f53acb954c5e
WayneCui <zhuxiaozhu8167@163.com>
2019-04-27 17:44:57 +0800

    FEAT: a more efficient way for 350.intersection-of-two-arrays-ii.go

--- 350.intersection-of-two-arrays-ii.go
+++ 350.intersection-of-two-arrays-ii.go
@@ -3,7 +3,7 @@
  *
  * [350] Intersection of Two Arrays II
  */
-func intersect(nums1 []int, nums2 []int) []int {
+func intersect1(nums1 []int, nums2 []int) []int {
 	m := len(nums1)
 	n := len(nums2)
 	var out []int
@@ -17,6 +17,31 @@ func intersect(nums1 []int, nums2 []int) []int {
 				cache[j] = 1
 				break
 			}
+		}
+	}
+
+	return out
+}
+
+func intersect(nums1 []int, nums2 []int) []int {
+	m := len(nums1)
+	n := len(nums2)
+	var out []int
+	cache := make(map[int]int)
+	for i := 0; i < m; i++ {
+		_, ok := cache[nums1[i]]
+		if ok {
+			cache[nums1[i]] = cache[nums1[i]] + 1
+		} else {
+			cache[nums1[i]] = 1
+		}
+	}
+
+	for j := 0; j < n; j++ {
+		count, ok := cache[nums2[j]]
+		if ok && count > 0{
+			out = append(out, nums2[j])
+			cache[nums2[j]] = cache[nums2[j]] - 1
 		}
 	}
 
