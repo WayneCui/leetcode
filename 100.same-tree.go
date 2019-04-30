531e9bedb3b93ac9c3de0771ec4a8798be3b3cd9
WayneCui <zhuxiaozhu8167@163.com>
2019-04-30 20:12:48 +0800

    FEAT: add 100.same-tree.go

---
+++ 100.same-tree.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=100 lang=golang
+ *
+ * [100] Same Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	} else {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+}
+
