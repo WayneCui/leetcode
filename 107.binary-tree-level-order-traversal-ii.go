d3c7917240a221b1dea84616ad72228e1eb61ff9
WayneCui <zhuxiaozhu8167@163.com>
2019-05-03 20:04:49 +0800

    FEAT: reverse the result at last

--- 107.binary-tree-level-order-traversal-ii.go
+++ 107.binary-tree-level-order-traversal-ii.go
@@ -11,7 +11,7 @@
  *     Right *TreeNode
  * }
  */
- func levelOrderBottom(root *TreeNode) [][]int {
+ func levelOrderBottom1(root *TreeNode) [][]int {
 	result := [][]int{}
 	if root == nil { return result }
 	
@@ -53,4 +53,43 @@ func insert(s [][]int, v []int) [][]int {
 	copy(s[1:], s[0:])
 	s[0] = v
     return s
+}
+
+func levelOrderBottom(root *TreeNode) [][]int {
+	result := [][]int{}
+	if root == nil {
+		return result
+	}
+	queue := [][]*TreeNode{ {root} }
+	flag := 0
+	for len(queue) > flag {
+		layer := queue[flag]
+		newLayer := []*TreeNode{}
+		layerVal := []int{}
+		for _, node := range layer {
+			if node != nil {
+				layerVal = append(layerVal, node.Val)
+				if node.Left != nil {
+					newLayer = append(newLayer, node.Left)
+				}
+				
+				if node.Right != nil {
+					newLayer = append(newLayer, node.Right)
+				}
+			}
+		}
+
+		result = append(result, layerVal)
+		if len(newLayer) > 0 {
+			queue = append(queue, newLayer)
+		}
+		flag += 1
+	}
+
+	for i := len(result)/2-1; i >= 0; i-- {
+		opp := len(result)-1-i
+		result[i], result[opp] = result[opp], result[i]
+	}
+
+	return result
 }
