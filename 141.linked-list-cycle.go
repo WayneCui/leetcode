33900b83865244e0e7d569cfea89f3c232e58fad
WayneCui <zhuxiaozhu8167@163.com>
2019-05-14 20:09:22 +0800

    FEAT: add 141.linked-list-cycle.go

---
+++ 141.linked-list-cycle.go
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=141 lang=golang
+ *
+ * [141] Linked List Cycle
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	fast := head
+	slow := head
+
+	for fast != nil &&  slow != nil {
+		fast = fast.Next
+		if fast != nil {
+			fast = fast.Next
+		}
+
+		slow = slow.Next
+
+		if fast == slow {
+			return true
+		}
+	}
+
+	return false
+}
+
