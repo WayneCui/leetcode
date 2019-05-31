4ffaf2d7318f36e03b69ae5c5d562eb54ae607f5
WayneCui <zhuxiaozhu8167@163.com>
2019-05-31 23:47:53 +0800

    FEAT: a more efficient way for 239.sliding-window-maximum.go

--- 239.sliding-window-maximum.go
+++ 239.sliding-window-maximum.go
@@ -3,7 +3,7 @@
  *
  * [239] Sliding Window Maximum
  */
-func maxSlidingWindow(nums []int, k int) []int {
+func maxSlidingWindow1(nums []int, k int) []int {
 	n := len(nums)
 	output := []int{}
 	if n == 0 { return output }
@@ -41,3 +41,51 @@ func (h *IntHeap) Pop() interface{} {
 	return x
 }
 
+// another way, credits to https://leetcode.com/problems/sliding-window-maximum/discuss/65881/O(n)-solution-in-Java-with-two-simple-pass-in-the-array
+func maxSlidingWindow(nums []int, k int) []int {
+	n := len(nums)
+	output := []int{}
+	if n == 0 { return output }
+
+	forward := make([]int, n)
+	backward := make([]int, n)
+
+	epoch := 1
+	currMax := 0
+	for i := 0; i < n; i++ {
+		if i < epoch * k {
+			currMax = max(currMax, nums[i])
+		} else {
+			currMax = nums[i]
+			epoch += 1
+		}
+		forward[i] = currMax
+	}
+
+	currMax = 0
+	epoch -= 1
+	for i := n - 1; i >= 0; i-- {
+		if i > epoch * k {
+			currMax = max(currMax, nums[i])
+		} else {
+			currMax = nums[i]
+			epoch -= 1
+		}
+
+		backward[i] = currMax
+	}
+
+	for i := 0; i < n - k + 1; i++ {
+		output = append(output, max(forward[i + k - 1], backward[i]))
+	}
+
+	return output
+}
+
+func max(i, j int) int {
+	if i < j {
+		return j
+	} else {
+		return i
+	}
+}
