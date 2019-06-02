7410c509f985c8176fb6bc7072252061606dd399
WayneCui <zhuxiaozhu8167@163.com>
2019-06-02 08:21:17 +0800

    FEAT: add 144.binary-tree-preorder-traversal.go

---
+++ 144.binary-tree-preorder-traversal.go
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=144 lang=golang
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+//recursive
+func preorderTraversal0(root *TreeNode) []int {
+	var output []int
+	return pTraversal(root, output)
+}
+
+func pTraversal(root *TreeNode, output []int) []int {
+	if root == nil { return output }
+	output = append(output, root.Val)
+	if root.Left != nil {
+		output = pTraversal(root.Left, output)
+	}
+
+	if root.Right != nil {
+		output = pTraversal(root.Right, output)
+	}
+
+	return output
+}
+
+//iterative
+func preorderTraversal(root *TreeNode) []int {
+	output := []int{}
+	if root == nil {
+		return output
+	}
+
+	stack := []*TreeNode { root }
+	var node *TreeNode
+	for len(stack) > 0 {
+		node, stack = pop(stack)
+		if node.Right != nil {
+			stack = push(stack, node.Right)
+		}
+		
+		if node.Left != nil {
+			stack = push(stack, node.Left)
+		}
+
+		output = append(output, node.Val)
+	} 
+
+	return output
+}
+
+func pop(a []*TreeNode) (poped *TreeNode, remained []*TreeNode) {
+	x, a := a[len(a)-1], a[:len(a)-1]
+	return x, a
+}
+
+func push(a []*TreeNode, node *TreeNode) []*TreeNode {
+	return append(a, node)
+}
+
+func peek(a []*TreeNode) *TreeNode {
+	return a[len(a) - 1]
+}
+
+func find(nodes []*TreeNode, target *TreeNode) bool {
+	n := len(nodes)
+	if n <= 0 { return false }
+	for i := n - 1; i >= 0; i-- {
+		if nodes[i] == target {
+			return true
+		}
+	}
+
+	return false
+}
+
