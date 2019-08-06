edee3ea3e288009ff8ed1c6ad98e2c89cdd34a27
WayneCui <zhuxiaozhu8167@163.com>
2019-08-06 20:12:33 +0800

    FEAT: add 129.sum-root-to-leaf-numbers.go

---
+++ go/129.sum-root-to-leaf-numbers.go
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=129 lang=golang
+ *
+ * [129] Sum Root to Leaf Numbers
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func sumNumbers(root *TreeNode) int {
+    if root == nil { return 0 }
+	collector := [][]int{}
+	init := []int{}
+	traversal(root, init, &collector)
+
+	sum := 0
+	for _, p := range(collector) {
+		sum += combine(p)
+	}
+
+	return sum
+}
+
+func traversal(root *TreeNode, path []int, collector *[][]int) {
+	if root.Left == nil && root.Right == nil {
+		p := make([]int, len(path))
+		copy(p, path)
+		p = append(p, root.Val)
+		*collector = append(*collector, p)
+		return 
+	}
+
+	path = append(path, root.Val)
+	if(root.Left != nil) {
+		traversal(root.Left, path, collector)
+	}
+
+	if(root.Right != nil) {
+		traversal(root.Right, path, collector)
+	}
+}
+
+func combine(p []int) int {
+	var buffer bytes.Buffer
+	for _, v := range(p) {
+		buffer.WriteString(strconv.Itoa(v))
+	}
+
+	if v, err := strconv.Atoi(buffer.String()); err == nil {
+		return v
+	}
+
+	return 0
+}
+
