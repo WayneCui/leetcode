5b85f9325ef3d380de5679f7cc37058323018eb4
WayneCui <zhuxiaozhu8167@163.com>
2019-08-13 19:59:32 +0800

    FEAT: add 19.remove-nth-node-from-end-of-list.go

---
+++ go/19.remove-nth-node-from-end-of-list.go
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=19 lang=golang
+ *
+ * [19] Remove Nth Node From End of List
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode {
+		Next: head,
+	}
+	first := dummy
+	second := dummy
+	for i := 1; i <= n + 1; i++ {
+		first = first.Next
+	}
+
+	for first != nil {
+		first = first.Next
+		second = second.Next
+	}
+
+	second.Next = second.Next.Next
+
+	return dummy.Next
+}
+
