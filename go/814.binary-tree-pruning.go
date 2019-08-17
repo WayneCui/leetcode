1455606c07aaa4637a7b174c4657853d3706bb19
WayneCui <zhuxiaozhu8167@163.com>
2019-08-17 22:40:00 +0800

    FEAT: add 814.binary-tree-pruning.go

---
+++ go/814.binary-tree-pruning.go
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=814 lang=golang
+ *
+ * [814] Binary Tree Pruning
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func pruneTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	//postorder
+	pruneTree(root.Left)
+	pruneTree(root.Right)
+    
+	if isLeaf(root.Left) && root.Left.Val == 0 {
+		root.Left = nil
+	}
+	if isLeaf(root.Right) && root.Right.Val == 0 {
+		root.Right = nil
+	}
+	
+	if isLeaf(root) && root.Val == 0 {
+		return nil
+	} else {
+		return root
+	}
+}
+
+func isLeaf(node *TreeNode) bool {
+	return node != nil && node.Left == nil && node.Right == nil
+}
+
