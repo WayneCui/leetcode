b3cec05ad4cee32886ad0b39ca4d5455d2903037
WayneCui <zhuxiaozhu8167@163.com>
2019-08-24 22:24:08 +0800

    FEAT: add 958.check-completeness-of-a-binary-tree.go

---
+++ go/958.check-completeness-of-a-binary-tree.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=958 lang=golang
+ *
+ * [958] Check Completeness of a Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isCompleteTree(root *TreeNode) bool {
+    if root == nil  { return true }
+    layer := []*TreeNode { root }
+
+    end := false
+    for len(layer) > 0 {
+        nextLayer := []*TreeNode {}
+        for _, node := range(layer) {
+            if node == nil {
+                end = true
+            } else {
+                if end {
+                    return false
+                }
+                nextLayer = append(nextLayer, node.Left)
+                nextLayer = append(nextLayer, node.Right)
+            }
+        }
+
+        layer = nextLayer
+    }
+
+    return true
+}
+
