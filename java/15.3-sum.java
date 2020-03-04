d1434afbcad40b1c2940d55b6a30a3ace14afc9f
WayneCui <zhuxiaozhu8167@163.com>
2020-03-04 20:47:18 +0800

    FEAT: another solution for 15.3-sum.java

--- java/15.3-sum.java
+++ java/15.3-sum.java
@@ -92,6 +92,50 @@ class Solution {
 
         return result;
     }
+
+    public List<List<Integer>> threeSum3(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(len < 3) { return result; }
+
+        Arrays.sort(nums);
+        Map<Integer, Integer> next = new HashMap<>();
+        next.put(nums[len - 1], len);
+        Map<Integer, Integer> prev = new HashMap<>();
+        prev.put(nums[0], -1);
+
+        int ni = 0, pi = 0;
+        for(int k = 0; k < len; k++) {
+            if(k > 0 && nums[k] != nums[k - 1]) {
+                prev.put(nums[k], pi);
+
+                ni = k;
+                next.put(nums[k - 1], ni);
+            }
+            pi = k;
+        }
+
+        for(int i = 0; i < len - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1]) { continue; }
+            int target = 0 - nums[i];
+            int low = i + 1, high = len - 1;
+            while(low < high) {
+                if(nums[low] + nums[high] > target) {
+                    high = prev.get(nums[high]);
+                } else if(nums[low] + nums[high] < target) {
+                    low = next.get(nums[low]);
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[low], nums[high]));
+
+                    high = prev.get(nums[high]);
+                    low = next.get(nums[low]);
+                }
+            }
+        }
+
+        return result;
+    }
 }
 // @lc code=end
 
