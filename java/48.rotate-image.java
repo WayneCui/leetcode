0b8f809afbc0deedae471ed504d06ffa0550cd07
WayneCui <zhuxiaozhu8167@163.com>
2020-03-08 10:59:49 +0800

    FEAT: add 48.rotate-image.java

---
+++ java/48.rotate-image.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        if(n == 1) { return; }
+        for(int i = 0; i < n / 2; i++) {
+            for(int j = i; j < n - i - 1; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+}
+// @lc code=end
+
